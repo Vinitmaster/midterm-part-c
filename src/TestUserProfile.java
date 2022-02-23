a5a2208061b5062eb7b63e0478a4ad8df7186ecb
vinit <vinitpatel2803@gmail.com>
2022-02-23 11:10:58 -0500

    object is added

---
+++ src/TestUserProfile.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class TestUserProfile {
+    public static void main(String args[]){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = scn.nextLine();
+        System.out.println("select a genre: ");
+        int g = scn.nextInt();
+        
+    }
+}
